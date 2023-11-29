a843bd3e828bb67e9cf20024940bebbacbc3e6c8
Zoltan The DM <zoltanthedm@gmail.com>
2023-11-29 01:43:59 -0800

    Races mostly completed

--- scripts/RaceCreator.js
+++ scripts/RaceCreator.js
@@ -10,15 +10,15 @@ class RaceCreator {
     }
 
     async MakeRaces(raceJson, compendiumCreator, setabilities=false){
-        let racePack = await compendiumCreator("-race", "Item");
+        this.racePack = await compendiumCreator("-race", "Item");
 
         this.holdem = [];
 
         for (let raceData of raceJson){
             try{
                 let itemTemp = await this.handleSingleRace(raceData, setabilities);
-                await racePack.importDocument(itemTemp);
-                console.log(`Done importing ${raceData.name} into ${racePack.collection}`);
+                await this.racePack.importDocument(itemTemp);
+                console.log(`Done importing ${raceData.name} into ${this.racePack.collection}`);
             }
             catch(error){
                 console.error(`Failed on race ${raceData.name}`)
@@ -27,13 +27,15 @@ class RaceCreator {
             }
         }
 
-        console.log(new Set(this.holdem))
+
+        let h = [...new Set(this.holdem)]
+        // console.log(h)
 
     }
 
     async handleSingleRace(raceData, setabilities){
 
-            console.log(raceData);
+            // console.log(raceData);
 
             let ds;
 
@@ -58,7 +60,7 @@ class RaceCreator {
                 return acc;
             }, "");
 
-            let advancement = this.makeAdvancements(raceData, setabilities)
+            let advancement = await this.makeAdvancements(raceData, setabilities)
 
             let item = {
                 name: raceData.name,
@@ -71,24 +73,39 @@ class RaceCreator {
                       walk: raceData.speed,
                       units: 'ft',
                     },
+                    senses: this.makeSenses(raceData),
                 },
             };
 
             return Item.create(item, {temporary: true, displaySheet:false});
     }
 
-    makeAdvancements(raceData, setabilities){
+    async makeAdvancements(raceData, setabilities){
 
-        let advancements = raceData.trait.reduce((acc, ele) => {
+        let advancements = await raceData.trait.reduce(async (acc, ele) => {
+
+            acc = await acc;
 
             if(ele.name == "Size"){
                 acc.push(this.makeSizeAdvancement(ele, raceData.size));
             }
-            if(/Languages?/i.test(ele.name)){
+            else if(/Languages?/i.test(ele.name)){
                 acc.push(this.makeLanguageAdvancement(ele));
             }
-            if(/darkvision?/i.test(ele.name)){
-                acc.push(this.makeLanguageAdvancement(ele));
+            else if(RaceCreator.isProficiencies(ele.text)){
+                acc.push(this.makeTraitAdvancement(ele));
+            }
+            //TODO
+            // else if(RaceCreator.isResistances(ele.text)){
+            //     acc.push(this.makeResAdvancement(ele));
+            // }
+            else if(RaceCreator.skipThis(ele.name)){
+                //do nothing
+            }
+            else{
+                //make a basic item for this
+                let granter = RaceCreator.getGrantItem(acc);
+                granter.configuration.items.push((await this.makeRaceItem(ele)).uuid);
             }
 
             return acc;
@@ -97,6 +114,249 @@ class RaceCreator {
         return advancements.concat([
             this.makeASIAdvancement(raceData.ability, setabilities),
         ]);
+    }
+
+    static getGrantItem(accumuated){
+        let foundGranter = accumuated.find(n => n.type == "ItemGrant");
+
+        if (foundGranter){
+            return foundGranter;
+        }
+
+        accumuated.push({
+            type: "ItemGrant",
+            configuration: {
+                items: [],
+            },
+            level: 0,
+            _id: randomID(16)
+        });
+
+        return accumuated.find(n => n.type == "ItemGrant");
+    }
+
+    makeSenses(raceData){
+        
+        let output = {};
+
+        let darkvision = raceData.trait.find(ele => RaceCreator.isDarkVision(ele.name));
+
+        if(darkvision){
+            let match = darkvision.text.match(/(?<range>\d+)/);
+
+            if (match){
+                output.darkvision = parseInt(match.groups['range']);
+            }
+            else{
+                console.warn(`Could not find darkvision range in '${darkvision.text}'`);
+            }
+        }
+
+        return output;
+    }
+
+    async makeRaceItem(element){
+
+        let featData = {
+            name: element.name,
+            type: "feat",
+            system: {
+                type: {value: "race"},
+                description: {value: element.text},
+            }
+        };
+
+        // console.log(featData)
+
+        let item = await Item.create(featData, {temporary: true, displaySheet:false});
+
+        // console.log("Here??")
+        // console.log(featData)
+
+        let imp = await this.racePack.importDocument(item);
+        console.log(`Done importing ${element.name} into ${this.racePack.collection}`);
+
+        return imp;
+    }
+
+    static isDarkVision(name){
+        return /darkvision?/gi.test(name)
+    }
+
+    static ProfRegEx = /You (?:(?:have)|(?:gain)) proficiency (?:(?:in)|(?:with)) (?<proficiencies>[^\.]+)/i
+    static isProficiencies(text){
+        return RaceCreator.ProfRegEx.test(text)
+    }
+
+    //certain elements can be skipped
+    static skipThis(name){
+        return /Age/i.test(name) ||
+            /Alignment/i.test(name) ||
+            /Description/i.test(name) ||
+            /Speed/i.test(name) ||
+            /Darkvision/i.test(name) ||
+            /Creature Type/i.test(name);
+    }
+
+
+
+    makeTraitAdvancement(traitData){
+        let profMatch = traitData.text.match(RaceCreator.ProfRegEx);
+
+        let choices = [];
+        let grants = [];
+
+        if (/your choice/.test(profMatch.groups['proficiencies'])){
+            function choiceMatcher(group, matcher, par={})
+            {
+                if(!par.countParcer){
+                    par.countParcer = match=>Parser._numberMap[match.groups['number']];
+                }
+                if(!par.poolParcer){
+                    par.poolParcer = match=>RaceCreator.spitNLList2(match.groups['pool']).map(RaceCreator.MapSkillNames);
+                }
+
+                let match = group.match(matcher);
+
+                if (!match){
+                    return [];
+                }
+
+                return [{
+                    count: par.countParcer(match),
+                    pool: par.poolParcer(match),
+                }];
+            }
+
+            choices = choices.concat(choiceMatcher(
+                profMatch.groups['proficiencies'],
+                /(?<number>\w+) of the following skills of your choice: (?<pool>.+)/
+            ));
+
+            choices = choices.concat(choiceMatcher(
+                profMatch.groups['proficiencies'],
+                /any combination of (?<number>\w+)/,
+                {poolParcer: n=>["skills:*", "tool:*"]}
+            ));
+
+            choices = choices.concat(choiceMatcher(
+                profMatch.groups['proficiencies'],
+                /^(?<number>\w+) skills? of your choice/,
+                {poolParcer: n=>["skills:*"]}
+            ));
+
+            choices = choices.concat(choiceMatcher(
+                profMatch.groups['proficiencies'],
+                /(?<number>\w+) tools? of your choice/,
+                {poolParcer: n=>["tool:*"]}
+            ));
+
+            choices = choices.concat(choiceMatcher(
+                profMatch.groups['proficiencies'],
+                /artisan's tools of your choice: (?<pool>.+)/,
+                {countParcer: n=>1}
+            ));
+
+            //TODO
+            //some special cases that I am too lazy to do
+            if (profMatch.groups['proficiencies'] == "Acrobatics or the Stealth skill (your choice)"){
+                choice.push({
+                    count: 1,
+                    pool: ["skills:acr", "skills:ste"]
+                });
+            }
+            else if (profMatch.groups['proficiencies'] == "Performance and Persuasion skills, and you have proficiency with one musical instrument of your choice"){
+                choice.push({
+                    count: 1,
+                    pool: ["tool:music:*"]
+                });
+                grants.push(["skills:prf", "skills:per"])
+            }
+        }
+        else{
+            function trimWords(words){
+                let output = words.replace(/skills?/, '');
+                output = output.replace(/the\s+/, '');
+                output = output.replace(/in\s+/, '');
+                output = output.replace(/\s+$/, '');
+                output = output.replace(/^\s+/, '');
+
+                return output;
+            }
+
+            grants = RaceCreator.spitNLList2(profMatch.groups['proficiencies']).map(
+                words=>RaceCreator.MapSkillNames(trimWords(words))
+            );
+        }
+
+
+
+
+        return {
+            _id: randomID(16),
+            type: "Trait",
+            configuration: {
+                grants: grants,
+                choices: choices,
+                hint: traitData.text
+            },
+            title: traitData.name,
+        };
+    }
+
+    static MapSkillNames(name){
+
+        const SkillMap = {
+            "acrobatics"            : "skills:acr",
+            "animal handling"       : "skills:ani",
+            "arcane"                : "skills:arc",
+            "athletics"             : "skills:ath",
+            "deception"             : "skills:dec",
+            "history"               : "skills:his",
+            "insight"               : "skills:ins",
+            "intimidation"          : "skills:itm",
+            "investigation"         : "skills:inv",
+            "medicine"              : "skills:med",
+            "nature"                : "skills:nat",
+            "perception"            : "skills:prc",
+            "performance"           : "skills:prf",
+            "persuasion"            : "skills:per",
+            "religion"              : "skills:rel",
+            "sleight of hand"       : "skills:slt",
+            "stealth"               : "skills:ste",
+            "survival"              : "skills:sur",
+            "battleaxe"             : "weapon:mar:battleaxe",
+            "handaxe"               : "weapon:sim:handaxe",
+            "light hammer"          : "weapon:sim:lighthammer",
+            "warhammer"             : "weapon:mar:warhammer",
+            "rapier"                : "weapon:mar:rapier",
+            "shortsword"            : "weapon:mar:shortsword",
+            "hand crossbow"         : "weapon:mar:handcrossbow",
+            "longsword"             : "weapon:mar:longsword",
+            "khopesh (longsword)"   : "weapon:mar:longsword",
+            "shortbow"              : "weapon:sim:shortbow",
+            "spear"                 : "weapon:sim:spear",
+            "longbow"               : "weapon:mar:longbow",
+            "javelin"               : "weapon:sim:javelin",
+            "smith's tools"         : "tool:art:smith",
+            "brewer's supplies"     : "tool:art:brewer",
+            "mason's tools"         : "tool:art:mason",
+            "artisan's tools (tinker's tools)": "tool:art:tinker",
+            "navigator's tools"     : "tool:navg",
+            "vehicles (water)"      : "tool:vehicle:water",
+            "thieves' tools"        : "tool:thief",
+            "poisoner's kit"        : "tool:pois",
+            "light"                 : "armor:lgt",
+            "medium armor"          : "armor:med",
+        }
+
+        let val = SkillMap[name.toLowerCase()]
+
+        if (!val){
+            console.warn(`Could not find mapped skill for ${name}`);
+        }
+
+        return val;
     }
 
     makeSizeAdvancement(sizeTrait, sizeLetter){
@@ -131,11 +391,7 @@ class RaceCreator {
 
         let configuration = {};
 
-        if (!langMatch){
-            console.warn("Failed on dong any language matching!");
-            console.log(languageTrait)
-        }
-        else{
+        if (langMatch){
             const langs = this.spitNLList(langMatch.groups['languages'])
 
             const LanguageMap = {
@@ -249,11 +505,27 @@ class RaceCreator {
 
     spitNLList(text){
         let match = text.split(/(?:\s+and\s+)|(?:,\s+and\s+)|(?:,\s+)/);
-        this.holdem = this.holdem.concat(match)
 
         return match
     }
 
+
+    static spitNLList2(text) {
+        let TwoItems = /(?<item1>.+) (?:(?:and)|(?:or)) (?<item2>.+)/
+        let ThreePlusItems = /(?<itemX>(?:(.+), ){2,})(?:(?:and)|(?:or)) (?<item2>.+)/
+
+        if (ThreePlusItems.test(text)){
+            let x = text.match(ThreePlusItems);
+            return x.groups['itemX'].split(', ').slice(0, -1).concat([x.groups['item2']])
+        }
+        else if (TwoItems.test(text)){
+            let x = text.match(TwoItems)
+            return [x.groups['item1'], x.groups['item2']]
+        }
+        else{
+            return [text]
+        }
+    }
 
 
     makeASIAdvancement(text, setabilities){
